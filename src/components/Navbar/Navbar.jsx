078ebcdbccb0150752a808d9ad00ec8fae42b533
Roshan Shrestha <shrtroshan289@gmail.com>
2024-02-19 17:34:46 +0545

    lower nav bar completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from './DarkMode';
  
+const Menu = [
+  {
+    id : 1,
+    name : "Home",
+    link: "/#",
+  },
+  {
+    id : 2,
+    name : "About",
+    link: "/#",
+  }
+]
+const DropdownLinks =[
+  {
+    id : 1,
+    name : "Home",
+    link: "/#",
+  },
+  {
+    id : 2,
+    name : "About",
+    link: "/#",
+  }
+]
 const Navbar = () => {
   return (
     <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
@@ -14,7 +38,7 @@ const Navbar = () => {
                 <a href="#"
                 className="font-bold text-2xl sm:text-3xl flex gap-2">
                     <img src={Logo} alt="Logo" 
-                    className="w-12 uppercase" />CakeBake
+                    className="w-12 uppercase" /><p className='mt-2'>CakeBake</p>
                 </a>
             </div>
             {/* Search bar */}
@@ -28,7 +52,8 @@ const Navbar = () => {
                         duration-300 rounded-full border 
                         border-gray-300 px-2 py-1 
                         focus:outline-none focus:border-1 
-                        focus:border-primary'
+                        focus:border-primary dark:boarder-gray-500
+                        dark:bg-gray-800'
                     />
                     <IoMdSearch 
                     className='text-gray-500
@@ -62,8 +87,36 @@ const Navbar = () => {
         </div>
       </div>
       {/* Lower Navbar */}
-      <div>
-
+      <div className='flex justify-center'>
+        <ul className='sm:flex hidden items-center gap-4'>
+          {
+            Menu.map((data) => (
+              <li key={data.id}>
+                <a href={data.link}
+                className='inline-block px-4 hover:text-primary duration-200'>{data.name}</a>
+              </li>
+            ))}
+            {/* dropdown menu */}
+            <li className='group relative cursor-pointer'>
+              <a href="#" 
+              className='flex items-center gap-[2px] scroll-py-2'>
+                Hover
+                <span>
+                  <FaCaretDown className='transition-all duration-200 group-hover:rotate-180'/>
+                </span>
+                </a>
+                <div>
+                  <ul className='absolute z-[9900] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md'>
+                    {DropdownLinks.map((data) => (
+                      <li key={data.id}>
+                        <a href={data.link}
+                        className='inline-block w-full rounded-md p-2 hover:bg-primary/20'>{data.name}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+            </li>
+        </ul>
       </div>
     </div>
   )
